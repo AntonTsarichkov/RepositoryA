f18c0448331b999dbd7d0299ebb21e78ad2b7819
AntonTsarichkov <145209967+AntonTsarichkov@users.noreply.github.com>
2023-12-12 12:53:13 +0200

    Add files via upload

---
+++ Lab4.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab4
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Ecosystem ecosystem = new Ecosystem();
+            ecosystem.AddOrganism(new Animal(100, 2, 10, 5));
+            ecosystem.AddOrganism(new Plant(50, 5, 3, 4));
+            ecosystem.AddOrganism(new Microorganism(20, 1, 1, 3));
+            Console.WriteLine($"{ecosystem.Organims.Count} initial quantity of organism");
+            for (int i = 0; i < 20; i++) 
+            {
+                ecosystem.Simulation();
+                Console.WriteLine($"{ecosystem.Organims.Count} organisms are alive after {i + 1} days");
+            }
+            Console.WriteLine("Simulation had finished!");
+        }
+    }
+}
